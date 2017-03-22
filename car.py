de48341773e27b5a1be4753b834b7c983253b450
Christian Blum <christianb314@gmail.com>
2017-03-22 13:46:59 +0100

    refactoring car

--- car.py
+++ car.py
@@ -2,22 +2,26 @@ from pygame.math import Vector2
 import math
 from matplotlib import pyplot as plt
 
-carLocation = Vector2(0.0, 0.0)
-carHeading = 0.0
-carSpeed = 1.0
-steerAngle = 0.0
-wheelBase = 1.0
-dt = 0.1
+class Car:
+  def __init__(self):
+    self.carLocation = Vector2(0.0, 0.0)
+    self.carHeading = 0.0
+    self.carSpeed = 1.0
+    self.steerAngle = 0.0
+    self.wheelBase = 1.0
+    self.frontWheel = self.carLocation + self.wheelBase/2.0 * Vector2(math.cos(self.carHeading), math.sin(self.carHeading) )
+    self.backWheel = self.carLocation - self.wheelBase/2.0 * Vector2(math.cos(self.carHeading), math.sin(self.carHeading) )
+  def update(self, dt):
+    self.frontWheel = self.carLocation + self.wheelBase/2.0 * Vector2(math.cos(self.carHeading), math.sin(self.carHeading) )
+    self.backWheel = self.carLocation - self.wheelBase/2.0 * Vector2(math.cos(self.carHeading), math.sin(self.carHeading) )
+    self.backWheel += self.carSpeed * dt * Vector2(math.cos(self.carHeading) , math.sin(self.carHeading))
+    self.frontWheel += self.carSpeed * dt * Vector2(math.cos(self.carHeading+self.steerAngle) , math.sin(self.carHeading+self.steerAngle))
+    self.carLocation = (self.frontWheel + self.backWheel)/2.0
+    self.carHeading = math.atan2( self.frontWheel.y - self.backWheel.y , self.frontWheel.x - self.backWheel.x )
+
+car = Car()
 
 for i in range(10):
-  frontWheel = carLocation + wheelBase/2.0 * Vector2(math.cos(carHeading), math.sin(carHeading) )
-  backWheel = carLocation - wheelBase/2.0 * Vector2(math.cos(carHeading), math.sin(carHeading) )
-
-  backWheel += carSpeed * dt * Vector2(math.cos(carHeading) , math.sin(carHeading))
-  frontWheel += carSpeed * dt * Vector2(math.cos(carHeading+steerAngle) , math.sin(carHeading+steerAngle))
-
-  carLocation = (frontWheel + backWheel)/2.0
-  carHeading = math.atan2( frontWheel.y - backWheel.y , frontWheel.x - backWheel.x )
-
-  plt.plot(carLocation.x, carLocation.y, "k.")
+  car.update(dt=0.1)
+  plt.plot(car.carLocation.x, car.carLocation.y, "k.")
 plt.show()
