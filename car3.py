b532608f9c51f8c75097ed67bfee4c836a73e481
Christian Blum <christianb314@gmail.com>
2017-03-22 19:20:47 +0100

    switched to pygame drawing

--- car3.py
+++ car3.py
@@ -1,24 +1,28 @@
 import pymunk
-import pymunk.matplotlib_util
-import matplotlib.pyplot as plt
+import pygame
+from pygame.locals import *
+from pymunk import pygame_util
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
 
 space = pymunk.Space()      # Create a Space which contain the simulation
-space.gravity = 0,-1000     # Set its gravity
+space.gravity = (0, 0)     # Set its gravity
 
-body = pymunk.Body(1,1666)  # Create a Body with mass and moment
-body.position = 50,100      # Set the position of the body
+body = pymunk.Body(1, pymunk.moment_for_box(1, (10,5)))  # Create a Body with mass and moment
+body.position = (300, 300)      # Set the position of the body
 
-poly = pymunk.Poly.create_box(body) # Create a box shape and attach to body
+poly = pymunk.Poly.create_box(body, (10, 5)) # Create a box shape and attach to body
 space.add(body, poly)       # Add both body and shape to the simulation
 
-plt.ion()
-for i in range(100):
-  space.step(0.01)
-  plt.clf()
-  plt.title(str(i))
-  drawer = pymunk.matplotlib_util.DrawOptions(plt.gca())
-  space.debug_draw(drawer)
-  plt.gca().set_xlim((0, 1000))
-  plt.gca().set_aspect("equal", "datalim")
-  plt.pause(1e-6)
-  plt.draw()
+draw_options = pygame_util.DrawOptions(screen)
+
+for i in range(1000):
+  if i < 200:
+    body.apply_force_at_local_point((0.0, 10.0), (10.0, 0.0))
+  
+  screen.fill((0, 0, 0))
+  space.debug_draw(draw_options)
+  space.step(1/100.0)
+  pygame.display.flip()
+  clock.tick(100)
