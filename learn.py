450b1aa98343600938f8d4bb8b2d1e3eef826d47
Christian Blum <christianb314@gmail.com>
2017-03-23 19:01:27 +0100

    now less motor command updates

--- learn.py
+++ learn.py
@@ -6,6 +6,7 @@ from pymunk.vec2d import Vec2d
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
+import xgboost as xgb
 
 class CarModel:
   def __init__(self, space, position):
@@ -54,43 +55,47 @@ class Car:
     self.brake = 0.0
     self.steer_angle = 0.0
     self.reward = 0.0
-    self.q_model = RandomForestRegressor()
+    self.q_model = xgb.XGBRegressor()
     self.log = []
     self.rewards = []
   def logging(self):
     row = [self.car_model.body.angle, self.car_model.body.angular_velocity, self.car_model.body.velocity.x, self.car_model.body.velocity.y, self.acceleration, self.steer_angle]
     self.log.append(row)
-    self.rewards.append([self.reward])
+    self.rewards.append(self.reward)
   def step(self):
+    acceleration_space = np.linspace(0.0, 100.0, 10)
+    steer_angle_space = np.linspace(-0.3, 0.3, 11)
     # calculate last reward
     self.reward = self.car_model.body.position.x - self.last_position.x
-    # epsilon-greedy
-    epsilon = np.random.rand(1)[0]
-    #epsilon
-    if epsilon < 1e-1 or len(self.log) < 500:
-      self.acceleration = np.random.choice(np.linspace(-100.0, 100.0, 5))
-      self.steer_angle = np.random.choice(np.linspace(-0.3, 0.3, 5))
-    # greedy
-    else:
-      if len(self.log) % 500 == 0:
-        # retrain model
-        self.q_model = RandomForestRegressor(n_estimators=100, n_jobs=4, min_samples_leaf=0.05)
-        self.q_model.fit(self.log, self.rewards)
-      # use model to decide on action
-      try:
-        search = []
-        for acceleration in np.linspace(-100.0, 100.0, 5):
-          for steer_angle in np.linspace(-0.3, 0.3, 5):
-            x = [[self.car_model.body.angle, self.car_model.body.angular_velocity, self.car_model.body.velocity.x, self.car_model.body.velocity.y, acceleration, steer_angle]]
-            q = q_model.predict(x)[0]
-            search.append([acceleration, steer_angle, q])
-        search.sort(key=lambda row: row[-1])
-        best = search[-1]
-        self.acceleration = best[0]
-        self.steer_angle = best[1]
-      except: # corner case for first training
-        self.acceleration = np.random.choice(np.linspace(-100.0, 100.0, 5))
-        self.steer_angle = np.random.choice(np.linspace(-0.3, 0.3, 5))
+    # don't decide every timestep
+    if len(self.log) % 10:
+      # epsilon-greedy
+      epsilon = np.random.rand(1)[0]
+      #epsilon
+      if epsilon < 1e-1 or len(self.log) < 500:
+        self.acceleration = np.random.choice(acceleration_space)
+        self.steer_angle = np.random.choice(steer_angle_space)
+      # greedy
+      else:
+        if len(self.log) % 500 == 0:
+          # retrain model
+          self.q_model = xgb.XGBRegressor(nthread=4)
+          self.q_model.fit(self.log, self.rewards)
+        # use model to decide on action
+        try:
+          search = []
+          for acceleration in acceleration_space:
+            for steer_angle in steer_angle_space:
+              x = [[self.car_model.body.angle, self.car_model.body.angular_velocity, self.car_model.body.velocity.x, self.car_model.body.velocity.y, acceleration, steer_angle]]
+              q = q_model.predict(x)[0]
+              search.append([acceleration, steer_angle, q])
+          search.sort(key=lambda row: row[-1])
+          best = search[-1]
+          self.acceleration = best[0]
+          self.steer_angle = best[1]
+        except: # corner case for first training
+          self.acceleration = np.random.choice(acceleration_space)
+          self.steer_angle = np.random.choice(steer_angle_space)
     # execute action
     self.car_model.drive(self.acceleration, self.brake, self.steer_angle)
     # update last_position and logging
@@ -111,7 +116,7 @@ if __name__ == "__main__":
   car = Car(space, (200, 400))
   
   plt.ion()
-  for i in range(100000):
+  for i in range(1000000):
     car.step()
     position_log.append(car.car_model.body.position)
 
@@ -125,7 +130,7 @@ if __name__ == "__main__":
       plt.draw()
 
     # physical simulation
-    space.step(1/50.0)
+    space.step(1/100.0)
     # drawing etc
 #    screen.fill((255, 255, 255))
 #    space.debug_draw(draw_options)
