8e99f43df293aea9abe7f2105842300ac95ec734
Christian Blum <christianb314@gmail.com>
2017-03-23 18:19:01 +0100

    changed drawing, switched to random forest

--- learn.py
+++ learn.py
@@ -4,8 +4,8 @@ from pygame.locals import *
 from pymunk import pygame_util
 from pymunk.vec2d import Vec2d
 import numpy as np
-from sklearn.svm import SVR
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
 
 class CarModel:
   def __init__(self, space, position):
@@ -54,6 +54,7 @@ class Car:
     self.brake = 0.0
     self.steer_angle = 0.0
     self.reward = 0.0
+    self.q_model = RandomForestRegressor()
     self.log = []
     self.rewards = []
   def logging(self):
@@ -63,61 +64,70 @@ class Car:
   def step(self):
     # calculate last reward
     self.reward = self.car_model.body.position.x - self.last_position.x
-    print "reward:", self.reward
     # epsilon-greedy
     epsilon = np.random.rand(1)[0]
     #epsilon
     if epsilon < 1e-1 or len(self.log) < 500:
-      print "epsilon"
       self.acceleration = np.random.choice(np.linspace(-100.0, 100.0, 5))
       self.steer_angle = np.random.choice(np.linspace(-0.3, 0.3, 5))
     # greedy
     else:
-      print "greedy"
-      # retrain model
-      if len(self.log) > 0:
-        scaler = StandardScaler()
-        xx = scaler.fit_transform(self.log)
-        q_model = SVR()
-        q_model.fit(xx, self.rewards)
-        # use model to decide on action
+      if len(self.log) % 500 == 0:
+        # retrain model
+        self.q_model = RandomForestRegressor()
+        self.q_model.fit(self.log, self.rewards)
+      # use model to decide on action
+      try:
         search = []
         for acceleration in np.linspace(-100.0, 100.0, 5):
           for steer_angle in np.linspace(-0.3, 0.3, 5):
             x = [[self.car_model.body.angle, self.car_model.body.angular_velocity, self.car_model.body.velocity.x, self.car_model.body.velocity.y, acceleration, steer_angle]]
-            xx = scaler.transform(x)
-            q = q_model.predict(xx)[0]
+            q = q_model.predict(x)[0]
             search.append([acceleration, steer_angle, q])
         search.sort(key=lambda row: row[-1])
         best = search[-1]
         self.acceleration = best[0]
         self.steer_angle = best[1]
+      except: # corner case for first training
+        self.acceleration = np.random.choice(np.linspace(-100.0, 100.0, 5))
+        self.steer_angle = np.random.choice(np.linspace(-0.3, 0.3, 5))
     # execute action
-    print "action:", self.acceleration, self.brake, self.steer_angle
     self.car_model.drive(self.acceleration, self.brake, self.steer_angle)
     # update last_position and logging
     self.last_position = Vec2d(self.car_model.body.position)
     self.logging()
 
 if __name__ == "__main__":
-  pygame.init()
-  screen = pygame.display.set_mode((1600, 800))
-  clock = pygame.time.Clock()
-  draw_options = pygame_util.DrawOptions(screen)
+#  pygame.init()
+#  screen = pygame.display.set_mode((1600, 800))
+#  clock = pygame.time.Clock()
+#  draw_options = pygame_util.DrawOptions(screen)
 
   space = pymunk.Space() 
   space.gravity = (0, 0)
-  
+ 
+  position_log = []
+
   car = Car(space, (200, 400))
   
+  plt.ion()
   for i in range(30000):
-    print "step:", i
     car.step()
-    
+    position_log.append(car.car_model.body.position)
+
+    if i % 500 == 0:
+      plt.clf()
+      plt.plot([p.x for p in position_log], [p.y for p in position_log], "k-")
+      plt.plot(position_log[-1].x, position_log[-1].y, "k.")
+      plt.gca().set_aspect("equal", "datalim")
+      plt.title(str(i))
+      plt.pause(1e-3)
+      plt.draw()
+
     # physical simulation
     space.step(1/50.0)
     # drawing etc
-    screen.fill((255, 255, 255))
-    space.debug_draw(draw_options)
-    pygame.display.flip()
-    clock.tick(50)
+#    screen.fill((255, 255, 255))
+#    space.debug_draw(draw_options)
+#    pygame.display.flip()
+#    clock.tick(50)
