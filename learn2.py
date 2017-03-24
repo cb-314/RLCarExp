d27412403a990b47c8082635cccd4fc3ad45716a
Christian Blum <christianb314@gmail.com>
2017-03-24 17:25:07 +0100

    got rid of errorous try except block

--- learn2.py
+++ learn2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import SGDRegressor
 import matplotlib.pyplot as plt
 import math
 import cPickle
@@ -40,27 +41,23 @@ class Car:
     # epsilon-greedy
     epsilon = np.random.rand(1)[0]
     #epsilon
-    if epsilon < 1e-2 or len(self.log) < 5000:
+    if epsilon < 1e-2 or len(self.log) < 500:
       self.steer_angle = np.random.choice(steer_angle_space)
     # greedy
     else:
-      if len(self.log) % 1000 == 0:
+      if len(self.log) % 100 == 0:
         # retrain model
-        self.q_model = KNeighborsRegressor(n_jobs=4)
+        self.q_model = RandomForestRegressor()
         self.q_model.fit(self.log, self.rewards)
       # use model to decide on action
-      try:
-        search = []
-        for steer_angle in steer_angle_space:
-          x = [self.velocity[0], self.velocity[1], self.steer_angle]
-          q = q_model.predict(x)[0]
-          search.append([steer_angle, q])
-        search.sort(key=lambda row: row[-1])
-        best = search[-1]
-        self.steer_angle = best[0]
-      except: # corner case for first training
-        print "except"
-        self.steer_angle = np.random.choice(steer_angle_space)
+      search = []
+      for steer_angle in steer_angle_space:
+        x = [[self.car_model.velocity[0], self.car_model.velocity[1], self.steer_angle]]
+        q = self.q_model.predict(x)[0]
+        search.append([steer_angle, q])
+      search.sort(key=lambda row: row[-1])
+      best = search[-1]
+      self.steer_angle = best[0]
     # update last_position and logging
     self.last_position = np.array(self.car_model.position)
     self.logging()
@@ -70,15 +67,16 @@ class Car:
 if __name__ == "__main__":
   position_log = []
 
-  car = Car(1e-2)
+  car = Car(1e-1)
   
   plt.ion()
   i = 0
   while True:
+    print i
     car.step()
     position_log.append(np.array(car.car_model.position))
 
-    if i % 1000 == 0:
+    if i % 100 == 0:
       plt.clf()
       plt.plot([p[0] for p in position_log], [p[1] for p in position_log], "k-")
       plt.plot(position_log[-1][0], position_log[-1][1], "k.")
